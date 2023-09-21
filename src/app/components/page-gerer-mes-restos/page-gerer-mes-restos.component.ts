c64140467f1276fb8c2440d4f27b6f496c3ef024
Lowick <loic.vango@sncf.fr>
2023-09-21 13:55:56 +0200

    code pour afficher review

--- src/app/components/page-gerer-mes-restos/page-gerer-mes-restos.component.ts
+++ src/app/components/page-gerer-mes-restos/page-gerer-mes-restos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AvisService } from 'src/app/services/avis.service';
 import { RestaurantService } from 'src/app/services/restaurant.service';
 
 @Component({
@@ -7,13 +8,23 @@ import { RestaurantService } from 'src/app/services/restaurant.service';
   templateUrl: './page-gerer-mes-restos.component.html',
   styleUrls: ['./page-gerer-mes-restos.component.css']
 })
-export class PageGererMesRestosComponent {
-  constructor(private fb: FormBuilder, private restaurantService:RestaurantService){}
+export class PageGererMesRestosComponent  {
+
+  reviews:any[] = []
+  constructor(private fb: FormBuilder, private restaurantService:RestaurantService, private avisService:AvisService){}
   pseudo: string | undefined = "'titi'";
   // Initialisez un compteur de pouces en l'air
   numberOfThumbsUp: number = 0;
   numberOfThumbsDown: number = 0;
   // modalComponent: any;
+ 
+  
+
+  ngOnInit():void{
+    this.avisService.getReview().subscribe(data => {
+      this.reviews=data;
+    });
+  }
 
   createForm: FormGroup = this.fb.group({
     name:[ '', Validators.required],
@@ -22,6 +33,7 @@ export class PageGererMesRestosComponent {
     categorie:['',[Validators.required]],
     groupe:['',[Validators.required]],
   })
+
 
   create(){
     const data={
@@ -38,5 +50,7 @@ export class PageGererMesRestosComponent {
   remove(id:number){
   this.restaurantService.remove(id)
  }
+
+
 
 }
