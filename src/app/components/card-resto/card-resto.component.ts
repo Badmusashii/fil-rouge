f242512de3ee5014c756299e7a5cab0f053847d5
Badmusashii <badmusashii@gmail.com>
2023-09-25 11:09:53 +0200

    commit du travaille

--- src/app/components/card-resto/card-resto.component.ts
+++ src/app/components/card-resto/card-resto.component.ts
@@ -10,16 +10,18 @@ import { Restaurant } from 'src/app/interfaces/restaurant.interface';
 })
 export class CardRestoComponent implements OnInit {
   pseudo: string | undefined = '';
-   @Input() restaurant!: any;
+  @Input() restaurant!: any;
   // Initialisez un compteur de pouces en l'air
   numberOfThumbsUp: number = 0;
   numberOfThumbsDown: number = 0;
   avisSubject: any;
+  hasClicked: boolean = false;
+  lastVote: 'up' | 'down' | null = null;
   // modalComponent: any;
   constructor(private modalComponent: ModalComponent) {}
 
   ngOnInit(): void {
-    console.log("avis de l enfant" + this.restaurant.reviews);
+    console.log('avis de l enfant' + this.restaurant.reviews);
   }
   openModal() {
     this.modalComponent.openModal();
@@ -28,12 +30,54 @@ export class CardRestoComponent implements OnInit {
   handleReviewSubmitted(review: string) {
     // Traitez l'avis soumis ici, par exemple, envoyez-le à un service.
     this.ajouterAvis(review);
-    
   }
 
   ajouterAvis(nouvelAvis: string) {
     // alert('coucou');
     // this.avis = [...this.avis,nouvelAvis];
-    
+  }
+
+  // voteUp() {
+  //   if (!this.hasClicked) {
+  //     this.numberOfThumbsUp++;
+  //     this.hasClicked = !this.hasClicked;
+  //   }
+  // }
+  // voteDown() {
+  //   if (!this.hasClicked) {
+  //     this.numberOfThumbsUp++;
+  //     this.hasClicked = !this.hasClicked;
+  //   }
+  // }
+
+  voteUp() {
+    if (this.lastVote === 'up') {
+      // Si le dernier vote était déjà un "pouce en haut", réduis le compteur
+      this.numberOfThumbsUp--;
+      this.lastVote = null; // Réinitialise le dernier vote
+    } else {
+      if (this.lastVote === 'down') {
+        // Si le dernier vote était un "pouce en bas", réduis ce compteur
+        this.numberOfThumbsDown--;
+      }
+      // Augmente le compteur du "pouce en haut"
+      this.numberOfThumbsUp++;
+      this.lastVote = 'up'; // Met à jour le dernier vote
+    }
+  }
+  voteDown() {
+    if (this.lastVote === 'down') {
+      // Si le dernier vote était déjà un "pouce en bas", réduis le compteur
+      this.numberOfThumbsDown--;
+      this.lastVote = null; // Réinitialise le dernier vote
+    } else {
+      if (this.lastVote === 'up') {
+        // Si le dernier vote était un "pouce en haut", réduis ce compteur
+        this.numberOfThumbsUp--;
+      }
+      // Augmente le compteur du "pouce en bas"
+      this.numberOfThumbsDown++;
+      this.lastVote = 'down'; // Met à jour le dernier vote
+    }
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
   constructor(private http: HttpClient, private router: Router) {}
-  login(data: { username: string; email: string; password: string }) {
+  login(data: { email: string; password: string }) {
     interface ApiResponse {
       accessToken: string;
     }
@@ -30,7 +30,7 @@ export class AuthService {
     return this.http.post(`http://localhost:8080/api/auth/register`, data);
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('token');
     this.router.navigate(['/login']);
   }
