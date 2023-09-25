46e345abbc0a285ef79950c95ab376b54d0ec516
MelinaSlhk <melinaloony@gmail.com>
2023-09-25 09:12:49 +0200

    si derniere modif

--- src/app/services/avis.service.ts
+++ src/app/services/avis.service.ts
@@ -32,4 +32,14 @@ getReview(): Observable<string[]> {
       reviewdata
     );
   }
+
+  // A faire apres la requete: enregistrer les vote dans le back!
+
+//    enregistrerVote(idResto: number, vote: boolean): Observable<any> {
+//     const voteData = {
+//       idResto: idResto,
+//       vote: vote,
+//     };
+ //return this.http.post<any>(`${this.apiUrl}/voter`, voteData); // Remplacez "/voter" par l'URL de votre endpoint de vote
+// }
 }
