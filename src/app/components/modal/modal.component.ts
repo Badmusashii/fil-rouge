c75655962d0b2486bef92d257b659f2eb06c1fee
MelinaSlhk <melinaloony@gmail.com>
2023-09-27 16:27:47 +0200

    ras

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -8,22 +8,24 @@ import { Restaurant } from 'src/app/interfaces/restaurant.interface';
   styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent {
-    // Appel du setter si la valeur transmise par le parent change
-  @Input() set idResto(value: { id: number, name: string }) {
+ 
+  // Appel du setter si la valeur transmise par le parent change
+  @Input() set idResto(value: { id: number; name: string }) {
     this._idResto = value.id;
     this.nameResto = value.name;
-            if (value !== undefined) {
+    if (value !== undefined) {
       this.openModal();
     }
   } // Ajoutez une variable pour suivre l'état de la modale
 
-    @Output() reviewSubmitted = new EventEmitter<string>();
+  @Output() reviewSubmitted = new EventEmitter<string>();
   reviewText: string = '';
   isModalOpen: boolean = false;
   _idResto: number | undefined;
- nameResto: string | undefined;
+  nameResto: string | undefined;
   constructor(private avisService: AvisService) {}
 
+  
   openModal() {
     this.isModalOpen = true; // Ouvrez la modale
   }
@@ -33,12 +35,13 @@ export class ModalComponent {
   }
 
   submitReview() {
-    // Créez un objet avec l'avis et un vote factice (par exemple, true).
+        // Créez un objet avec l'avis et un vote factice (par exemple, true).
     let reviewData = {
       review: this.reviewText,
       vote: true, // Vous devrez déterminer comment gérer le vote ici.
       idResto: String(this._idResto),
     };
+    
     // Enregistrez l'avis dans le backend via un service (AvisService) ici.
     this.avisService.ajouterAvis(reviewData).subscribe(() => {
       // Une fois l'avis enregistré, émettez l'événement pour le faire apparaître dans la card.
@@ -46,5 +49,8 @@ export class ModalComponent {
       this.reviewText = ''; // Réinitialisez le champ de texte après la soumission.
       this.closeModal(); // Fermez la modale après la soumission.
     });
+
+    
+
   }
 }
